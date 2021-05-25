ac1d14537971acc90e157a5b1527e02f8ce1dd1d
PatrickJosephEns <patens6@gmail.com>
2021-05-25 12:29:13 +1200

    Fixed navbar and have content showing in content page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,31 +2,37 @@ import React, {Component} from 'react';
 
 function NavBar()  {
   return (
-          <nav class="navbar navbar-expand-lg navbar-dark primary-color">
-            <a class="navbar-brand" href="http://localhost:3000/">PJE</a>
-            <button class="navbar-toggler" type="button" 
-            data-toggle="collapse" data-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" 
-              aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="http://localhost:3000/">
-                    Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="http://localhost:3000/content">
-                    Content</a>   
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="http://localhost:3000/about">
-                    About</a>
-                </li>
-                </ul>
-            </div>
-          </nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="http://localhost:3000/">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="http://localhost:3000/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://localhost:3000/content" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Collections
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="http://localhost:3000/content">New Arrivals</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Contact Us</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">About</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Sign In</button>
+    </form>
+  </div>
+</nav>
   );
   }
  
--- client/src/components/cards/Cards.js
+++ client/src/components/cards/Cards.js
@@ -2,44 +2,53 @@ import React from 'react';
 import './Cards.css';
 import CardItem from './CardItems';
 
+
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Check out the latest collection!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
+              src='images/1.jpg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              label='New'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='images/2.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              label='New'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='images/3.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              label='New'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='/images/4.jpg'
               text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              label='New'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='images/5.jpg'
               text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
+              label='New'
               path='/sign-up'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/6.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='New'
+              path='/services'
             />
           </ul>
         </div>
