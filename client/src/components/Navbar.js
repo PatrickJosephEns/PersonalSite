43f8c31167fd6d62fbdcea89204e6382a3951c9a
Patrick Ens <pat@patricks-mbp.lan>
2021-11-23 15:45:47 +1300

    fixed nav links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,7 @@
 function NavBar()  {
   return (
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-  <a class="navbar-brand" href="http://localhost:3000/">patens</a>
+  <a class="navbar-brand" href="/">patens</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -10,17 +10,17 @@ function NavBar()  {
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="http://localhost:3000/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Collections
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="http://localhost:3000/content">New Arrivals</a>
+          <a class="dropdown-item" href="/content">New Arrivals</a>
           <a class="dropdown-item" href="#">Another page</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="http://localhost:3000/contact">Contact Us</a>
+          <a class="dropdown-item" href="/contact">Contact Us</a>
         </div>
       </li>
       <li class="nav-item">
